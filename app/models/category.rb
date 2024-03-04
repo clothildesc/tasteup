7c117e812a318f1463a98895231613ddabd60abc
Pierre Urban <pierreurban1@gmail.com>
2024-03-04 18:04:08 +0100

    added one recipe in the seed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
-  belongs_to :recipe_category
+  has_many :recipe_categories, dependent: :destroy
+  has_many :recipes, through: :recipe_categories
   validates :name, presence: true
 end
