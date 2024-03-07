68608a081c800fd142bfc9987eed815e63aac353
clothilde scache <clothilde.sca@gmail.com>
2024-03-07 14:12:53 +0100

    started coding show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_ingredients = @recipe.recipe_ingredients.includes(:ingredient)
   end
 
   def new
