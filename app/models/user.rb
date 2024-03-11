b26596a1f04f644f01f8944ccd1137373ee7151a
clothilde scache <clothilde.sca@gmail.com>
2024-03-11 16:59:15 +0100

    set gem favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_recipes, through: :favorites, source: :recipe
-  has_many :followers, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
-  has_many :followees, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
-  
-  has_many :followed_users, through: :followers, source: :followee
-  has_many :following_users, through: :followees, source: :follower
+
+  acts_as_favoritable
+  acts_as_favoritor
+
+  # has_many :followers, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
+  # has_many :followees, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
+
+  # has_many :followed_users, through: :followers, source: :followee
+  # has_many :following_users, through: :followees, source: :follower
 end
